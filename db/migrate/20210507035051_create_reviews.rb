0a508d6ffc88d944fec9677e6bf2651619b87e6e
Thomas Goldsbury <goldsbury.thomas@hotmail.com>
2021-05-07 12:27:03 +0800

    Updated

--- db/migrate/20210507035051_create_reviews.rb
+++ db/migrate/20210507035051_create_reviews.rb
@@ -1,8 +1,11 @@
 class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
-
+      t.bigint 'list_id', null: false
+      t.text 'comment'
+      t.integer 'rating'
       t.timestamps
+      t.index ['list_id'], name: 'index_reviews_on_list_id'
     end
   end
 end
